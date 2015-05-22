2c0579ee50a2a4e6df1d4319d001157b0042a870
axiomiety <axiomiety@gmail.com>
2015-05-23 07:05:11 +0800

    updated hamming with inject

--- hamming.rb
+++ hamming.rb
@@ -1,10 +1,6 @@
 
 class Hamming
   def self.compute (seq1, seq2) # self to make it static o_O
-    dist = 0
-    seq1.each_char.zip(seq2.each_char) do |s1,s2| # strings aren't lists in Ruby!
-      dist += s1 != s2 ? 1 : 0
-    end
-    return dist
+    seq1.each_char.zip(seq2.each_char).inject(0) { |memo, s| s[0] == s[1] ? memo : memo+1 }
   end
 end
