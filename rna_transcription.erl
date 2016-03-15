db91863a7c6cab238e8e558c35c04c765dde737f
axiomiety <axiomiety@gmail.com>
2016-03-15 03:08:38 +0000

    v2 of rna transcription

--- rna_transcription.erl
+++ rna_transcription.erl
@@ -1,12 +1,11 @@
 -module(rna_transcription).
 -export([to_rna/1]).
 
-to_rna([Nucleotide]) ->
-  complement([Nucleotide]);
+to_rna([]) -> [];
 to_rna([Nucleotide|Tail]) ->
-  complement([Nucleotide]) ++ to_rna(Tail).
+  [complement(Nucleotide)|to_rna(Tail)].
 
-complement("G") -> "C";
-complement("C") -> "G";
-complement("T") -> "A";
-complement("A") -> "U".
+complement($G) -> $C;
+complement($C) -> $G;
+complement($T) -> $A;
+complement($A) -> $U.
