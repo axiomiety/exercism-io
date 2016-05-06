9be37cb66fc7d66cadf8233dc89bc683a365914f
axiomiety <axiomiety@gmail.com>
2016-05-06 22:21:38 +0000

    anagram_erlang

--- anagram.erl
+++ anagram.erl
@@ -1,15 +1,7 @@
 -module(anagram).
 -export([find/2]).
 
-find(Word, Ln) -> find(Word, Ln, []).
-
-find(_, [], Acc) -> lists:reverse(Acc);
-find(Word, [Word|T], Acc) -> find(Word, T, Acc); % same word doesn't count
-find(Word, [H|T], Acc) ->
-  case isanagram(Word, H) of
-    true  -> find(Word, T, [H|Acc]);
-    false -> find(Word, T, Acc)
-  end.
+find(Word, Ln) -> lists:filter(fun(W) -> isanagram(Word, W) end, Ln).
 
 isanagram(W1, W2) ->
   (lists:sort(string:to_lower(W1)) == lists:sort(string:to_lower(W2))) and (string:to_lower(W1) /= string:to_lower(W2)).
